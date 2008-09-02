1fb94714ec21da0909a1284c4cd29ced5736ac5f
Jim Pick <jim@jimpick.com>
2008-09-02 07:28:50 -0700

    Add script to upload demo

---
+++ upload-demo.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+scp \
+	contributors/JimPick/verticals/clientside-storage/clientside-storage.html \
+	contributors/JimPick/verticals/clientside-storage/TiddlySaver.jar \
+	jim@jimpick.com:~/test
